e41f2b0e20636919b2bd166c430570f0b5d3e640
ehhaines <haines.e@northeastern.edu>
2022-03-09 12:58:56 -0500

    Implemented all features

--- script.js
+++ script.js
@@ -54,35 +54,62 @@ function drawPoint(x_coor, y_coor) {
   ctx.stroke();
 }
 
-function findPi(num_points) {
-  let r_width = width / 2.5;
-  let r_height = height / 2.5;
-  let radius = width > height ? r_height : r_width;
 
-  let count = 0;
-  let red = 0;
-  let green = 0;
 
-  while (count < num_points) {
-    let x = (Math.random() * 2 - 1) * radius;
-    let y = (Math.random() * 2 - 1) * radius;
-    if (Math.sqrt(x*x + y*y) <= radius) {
-      ctx.strokeStyle = 'rgb(50, 255, 50)';
-      green++;
-    }
-    else {
-      ctx.strokeStyle = 'rgb(255, 50, 50)';
-      red++;
-    }
-    drawPoint(x, y);
-    count++;
+const button = document.getElementById("button");
+let global_count = 0;
+let global_green = 0;
+let global_pi = 0;
+let tracker = 0;
+
+button.onclick = function() {
+  if (tracker) {
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(0, 0, width, height);
+    drawCircle();
+    drawSquare();
+    document.getElementById("green").innerText = "GREEN: ";
+    document.getElementById("total").innerText = "TOTAL: ";
+    document.getElementById("pi").innerText = "PI = ";
   }
-  let pi = 4 * green / count
-  console.log(pi);
-  return pi;
+  let num_points = document.getElementById("entry").value;
+  num_points = parseInt(num_points);
+  if (num_points) {
+    let r_width = width / 2.5;
+    let r_height = height / 2.5;
+    let radius = width > height ? r_height : r_width;
+
+    let count = 0;
+    let red = 0;
+    let green = 0;
+
+    while (count < num_points) {
+      let x = (Math.random() * 2 - 1) * radius;
+      let y = (Math.random() * 2 - 1) * radius;
+      if (Math.sqrt(x*x + y*y) <= radius) {
+        ctx.strokeStyle = 'rgb(50, 255, 50)';
+        green++;
+      }
+      else {
+        ctx.strokeStyle = 'rgb(255, 50, 50)';
+        red++;
+      }
+      drawPoint(x, y);
+      count++;
+    }
+
+    document.getElementById("green").textContent += green;
+    document.getElementById("total").textContent += count;
+    let pi = 4 * green / count
+    document.getElementById("pi").textContent += pi;
+    console.log(pi);
+    global_count = count;
+    global_green = green;
+    global_pi = pi;
+    tracker = 1;
+    return pi;
+  }
 }
 
 drawCircle();
 drawSquare();
-
-findPi(10000);
