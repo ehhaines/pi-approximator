a9ec5bc3bab76dadf95da863ff675c8c05fa708b
ehhaines <haines.e@northeastern.edu>
2022-03-09 04:05:36 -0500

    Created text entry box for number of points

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
+const width = canvas.width = window.innerWidth - 160;
 const height = canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
